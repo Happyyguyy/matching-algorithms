5689cf594898f30fcb3772d3965a177b51d196e7
happyyguyy <chodr@bc.edu>
2019-03-15 22:21:20 -0400

    optimized DA_algorithm

--- DA_algorithm.py
+++ DA_algorithm.py
@@ -33,59 +33,51 @@ class SetofAgents(set):
 
 def DA_algorithm(proposers, receivers):
 
+    # closure to handle min comparison
+    def return_scope(agent):
+        def get_index(item):
+            return agent.pref.index(item)
+        return get_index
+
     matches = set()  #set of final matches
     while proposers:
-        proposals = []
         for receiver in receivers:
-            proposal_set = {}
-            proposal_set["receiver"] = receiver
 
-            proposing = []
+            suitors = []
             for each in proposers:
                 if each._pref[0] == receiver:
-                    proposing.append(each)
-            if proposing:
-                proposal_set["suitors"] = proposing
-                proposals.append(proposal_set)
+                    suitors.append(each)
 
-        for proposal_set in proposals:
-            # var declaration to make my life easier
-            receiver = proposal_set["receiver"]
-            suitors = proposal_set["suitors"]
             current_match = receiver.match
 
+            if suitors:
+                # remove current match (if it exists from receiver) and add to list:suitors
+                if current_match:
+                    suitors.append(current_match)    #  add to list:suitors
+                    receiver.match = None            #  set agent:receiver's match to None
+                    current_match.match = None       #  set the removed match's match to None
+                    proposers.add(current_match)     #  return current_match back to set of all proposers
 
-            # remove current match (if it exists from receiver) and add to list:suitors
-            if current_match:
-                suitors.append(current_match)    #  add to list:suitors
-                receiver.match = None            #  set agent:receiver's match to None
-                current_match.match = None       #  set the removed match's match to None
-                proposers.add(current_match)     #  return current_match back to set of all proposers
+                # set closure
+                pref_index = return_scope(receiver)
 
-            # closure to handle min comparison
-            def return_scope(agent):
-                def get_index(item):
-                    return agent.pref.index(item)
-                return get_index
-            pref_index = return_scope(proposal_set["receiver"])
+                # min comparison for best match
+                best_match = min(suitors, key=lambda x: pref_index(x))
 
-            # min comparison for best match
-            best_match = min(proposal_set["suitors"], key=lambda x: pref_index(x))
+                # set agent:suitors' and agent:match match to proposal_set other
+                receiver.match, best_match.match = best_match, receiver
 
-            # set agent:suitors' and agent:match match to proposal_set other
-            receiver.match, best_match.match = best_match, receiver
+                # Remove match from set:proposers and from list:suitors
+                proposers.remove(best_match)
+                suitors.remove(best_match)
 
-            # Remove match from set:proposers and from list:suitors
-            proposers.remove(best_match)
-            suitors.remove(best_match)
 
-            # Remove preference from list of rejected suitors
-            for suitor in suitors:
-                removed_pref = suitor._pref.pop(0)
-                #  if agent:suitor has no preference left then match with itself, remove from set:proposers, and add to return set:matches
-                if (not suitor._pref) or removed_pref == suitor:
-                    suitor.match = suitor
-                    matches.add((suitor, suitor.match))
+                # Remove preference from list of rejected suitors
+                for suitor in suitors:
+                    removed_pref = suitor._pref.pop(0)
+                    #  if agent:suitor has no preference left then match with itself, remove from set:proposers, and add to return set:matches
+                    if (not suitor._pref) or removed_pref == suitor:
+                        matches.add((suitor, suitor))
 
 
     for each in receivers:
